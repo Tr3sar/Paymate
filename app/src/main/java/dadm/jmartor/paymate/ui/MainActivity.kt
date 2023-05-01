48e8719277dccefb6aa5da11b3c60377e2d8589d
tr3sar <josepcdg@gmail.com>
2023-05-01 18:48:28 +0200

    Nueva carpeta .ui

--- app/src/main/java/dadm/jmartor/paymate/ui/MainActivity.kt
+++ app/src/main/java/dadm/jmartor/paymate/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package dadm.jmartor.paymate
+package dadm.jmartor.paymate.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dadm.jmartor.paymate.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
