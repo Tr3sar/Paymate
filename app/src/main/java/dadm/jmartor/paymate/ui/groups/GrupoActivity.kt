cebe45f2ac3155fee801e3a3452dc76a69d35346
tr3sar <josepcdg@gmail.com>
2023-05-11 17:48:19 +0200

    Actividad y Fragmentos para implementar gastos y deudas

---
+++ app/src/main/java/dadm/jmartor/paymate/ui/groups/GrupoActivity.kt
@@ -0,0 +1,36 @@
+package dadm.jmartor.paymate.ui.groups
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import dadm.jmartor.paymate.R
+import dadm.jmartor.paymate.databinding.ActivityGrupoBinding
+
+class GrupoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGrupoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityGrupoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_grupo)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_gastos, R.id.navigation_deudas
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/dadm/jmartor/paymate/ui/groups/ui/gastos/GastosFragment.kt
@@ -0,0 +1,42 @@
+package dadm.jmartor.paymate.ui.groups.ui.gastos
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import dadm.jmartor.paymate.databinding.FragmentGastosBinding
+
+class GastosFragment : Fragment() {
+
+    private var _binding: FragmentGastosBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(GastosViewModel::class.java)
+
+        _binding = FragmentGastosBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
