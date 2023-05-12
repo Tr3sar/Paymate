0a2fc53b6e079483b0df48b48be3db7b4255ff54
Ramses502 <ramses21793@gmail.com>
2023-05-12 21:08:32 +0200

    Seguimos avanzando en newgroup

--- app/src/main/java/dadm/jmartor/paymate/ui/newgroup/NewGroupFragment.kt
+++ app/src/main/java/dadm/jmartor/paymate/ui/newgroup/NewGroupFragment.kt
@@ -1,5 +1,6 @@
 package dadm.jmartor.paymate.ui.newgroup
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -13,10 +14,19 @@ class NewGroupFragment : Fragment(R.layout.fragment_new_group) {
     private var _binding : FragmentNewGroupBinding? = null
     private val binding get() = _binding!!
     private val viewModel: NewGroupViewModel by activityViewModels()
+    private var listMembers: List<String> = emptyList()
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentNewGroupBinding.bind(view)
+
+        binding.addMember.setOnClickListener {
+            var members: String = binding.members.text.toString()
+            var member: String = binding.personName.text.toString()
+            binding.members.text = members + member + "\n"
+            listMembers.plus(member)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/dadm/jmartor/paymate/ui/newgroup/NewGroupViewModel.kt
+++ app/src/main/java/dadm/jmartor/paymate/ui/newgroup/NewGroupViewModel.kt
@@ -1,5 +1,7 @@
 package dadm.jmartor.paymate.ui.newgroup
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -7,4 +9,13 @@ import javax.inject.Inject
 @HiltViewModel
 class NewGroupViewModel @Inject constructor():ViewModel(){
 
+    private val _members: MutableLiveData<Boolean> = MutableLiveData<Boolean>()
+    val members : LiveData<Boolean>
+        get() {
+            return _members
+        }
+
+    fun addMember(name:String){
+
+    }
 }
